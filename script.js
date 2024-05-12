35f960f412c52c1822eb2574a9157122c06a381b
JustaUser <67065522+Jass60131@users.noreply.github.com>
2024-05-12 23:44:40 +0200

    Prima Prova in assoluto
    versione 0.1

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Cambia lo sfondo della pagina
+    document.body.style.backgroundColor = '#333333'; // Cambia lo sfondo in grigio scuro
+    document.body.style.color = '#ffffff'; // Cambia il colore del testo in bianco
+});
+
+document.getElementById('enterText').addEventListener('click', function() {
+    // Mostra il contenuto nascosto
+    document.getElementById('contentToReveal').style.display = 'block';
+    // Nascondi il testo "Click to enter"
+    this.style.display = 'none';
+
+    // Nomi delle immagini di sfondo (esempio)
+    const imageNames = ['background_1.jpg', 'background_2.jpg', 'background_3.png', 'background_4.png'];
+
+    // Cartella contenente le immagini di sfondo (esempio)
+    const folderPath = 'img/';
+
+    // Genera un URL casuale per l'immagine di sfondo
+    const randomIndex = Math.floor(Math.random() * imageNames.length);
+    const randomImageName = imageNames[randomIndex];
+    const randomImageUrl = folderPath + randomImageName;
+
+    // Imposta l'immagine di sfondo
+    document.body.style.backgroundImage = `url('${randomImageUrl}')`;
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundRepeat = 'no-repeat';
+});
